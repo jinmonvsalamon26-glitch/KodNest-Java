95f34156262100c2f4a8e980ed777c20cce7ed99
jinmonvsalamon26-glitch <jinmonvsalamon26@gmail.com>
2026-08-11 08:05:32 +0530

    Display

---
+++ track/M02/introToJava/CalculateIntrest.java
@@ -0,0 +1,29 @@
+
+public class CalculateIntrest {
+
+    public static void main(String[] args) {
+        double principle = 10000;
+        double rate = 6.5;
+        double time = 2.0;
+        double simpleintrest = principle * rate * time / 100.0;
+        double totalAmount = principle + simpleintrest;
+        System.out.println("Simple Intrest: " + simpleintrest);
+        System.out.println("Total Amount: " + totalAmount);
+
+        double weight = 72.0;
+        double height = 1.8;
+        double bmi = weight / (height * height);
+        System.out.println("BMI: " + bmi);
+
+        int mark1 = 78;
+        int mark2 = 84;
+        int mark3 = 69;
+        int mark4 = 91;
+        int mark5 = 88;
+        int totalMarks = mark1 + mark2 + mark3 + mark4 + mark5;
+        double percentage = (double) totalMarks * 100.0 / 500;
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage);
+
+    }
+}
