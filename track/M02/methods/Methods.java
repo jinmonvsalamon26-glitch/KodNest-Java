563a3290747b346c09bfb5f899f6224993a7b917
jinmonvsalamon26-glitch <jinmonvsalamon26@gmail.com>
2026-08-12 07:47:52 +0530

    Display

---
+++ track/M02/methods/Methods.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+class MethodDemo {
+
+    void sayHello() {
+        System.out.println("Hello from a method!");
+    }
+
+    void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    int getLuckyNumber() {
+        return 7;
+    }
+
+    int add(int first, int second) {
+        return first + second;
+    }
+}
+
+class Methods {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        MethodDemo obj = new MethodDemo();
+        obj.sayHello();
+        obj.greet(name);
+        int luckyNumber = obj.getLuckyNumber();
+        System.out.println("Lucky_ Number:" + luckyNumber);
+        int sum = obj.add(first, second);
+        System.out.println("Sum:" + sum);
+    }
+}
