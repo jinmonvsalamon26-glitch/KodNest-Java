730cbd97d0d968bf877d5db53e332504ef4a3d63
jinmonvsalamon26-glitch <jinmonvsalamon26@gmail.com>
2026-08-14 12:38:24 +0530

    Display

---
+++ track/M02/constructor/ConstructorOverloading.java
@@ -0,0 +1,40 @@
+ class Student {
+    String name;
+    int age ;
+    double height;
+   
+    Student()
+    {
+        name = "Anu";
+        age = 19;
+        height = 5.0;
+
+    }
+    Student(String name)
+    {
+        this.name= name;
+    }
+
+    Student(String name,int age,double height)
+    {
+        this.name=name;
+        this.age=age;
+        this.height=height;
+    }
+    void display()
+    {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+}
+}
+public class ConstructorOverloading{
+    public static void main(String[]args){
+Student s2 = new Student();
+s2.display();
+Student s3 = new Student("Manu");
+s3.display();
+Student s1 = new Student();
+s1.display();
+    }
+}
