fa1b18db419d59c9f785a806e2f3ec6987fa5b2d
jinmonvsalamon26-glitch <jinmonvsalamon26@gmail.com>
2026-09-09 16:51:38 +0530

    Display

---
+++ track/M04/StringFundamentals/StringComparison.java
@@ -0,0 +1,12 @@
+
+public class StringComparison {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String res = s1.toUpperCase();
+        System.out.println(s1.toUpperCase());
+        System.out.println(res);
+        System.out.println(s1);
+
+    }
+}
