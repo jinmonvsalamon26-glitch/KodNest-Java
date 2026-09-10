20b70b6019df08a7cf69674dcba41f784a54784f
jinmonvsalamon26-glitch <jinmonvsalamon26@gmail.com>
2026-09-10 16:49:28 +0530

    Display

---
+++ track/M04/StringFundamentals/StringMethode1.java
@@ -0,0 +1,15 @@
+
+public class StringMethode1 {
+
+    public static void main(String[] args) {
+
+        String s1 = "raja";
+        String s2 = "Rani";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+        String t1 = new String("Anu");
+        String t2 = new String("kumar");
+        String res = t1.concat(t2);
+        System.out.println(res);
+    }
+}
---
+++ track/M04/StringFundamentals/StringMethode2.java
@@ -0,0 +1,7 @@
+
+public class StringMethode2 {
+
+    public static void main(String[] args) {
+        String s1 = "java";
+    }
+}
