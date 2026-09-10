652801631bdc02583ecfe002f66b5996ce87a0e5
jinmonvsalamon26-glitch <jinmonvsalamon26@gmail.com>
2026-09-10 17:42:36 +0530

    Display

---
+++ track/M04/StringBuilderAndStringProblems/RevStr.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class RevStr {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str = scanner.next();
+        char arr[] = str.toCharArray();
+        char newArr[] = new char[arr.length];
+        int j = newArr.length - 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            newArr[j] = arr[i];
+            j--;
+        }
+        String revStr = new String(newArr);
+        System.out.println("Original String : " + str);
+        System.out.println("Reversed String : " + revStr);
+    }
+}
